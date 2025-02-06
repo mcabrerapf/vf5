b568e3b20837bc51e0c00cab27bbde491d0e67bb
Manuel <mcabrerapf@gmail.com>
2025-02-07 00:47:48 +0100

    added copy on crash

--- src/Components/ErrorBoundry/ErrorBoundry.js
+++ src/Components/ErrorBoundry/ErrorBoundry.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { ALL_DATA_KEY } from "../../constants";
+import { getFromLocal } from "../../helpers";
 
 function ErrorFallback({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert">
-      <h2>Oops! Something went wrong.</h2>
-      <p>{error.message}</p>
-      <button onClick={resetErrorBoundary}>Try Again</button>
-    </div>
-  );
+    const copyToClipboard = () => {
+        const allCharacterData = getFromLocal(ALL_DATA_KEY);
+        if (!allCharacterData) return;
+        copyToClipboard(allCharacterData);
+    }
+    return (
+        <div role="alert">
+            <h2>Oops! Something went wrong.</h2>
+            <p>{error.message}</p>
+            <button onClick={copyToClipboard}>Copy to clipboard</button>
+            <button onClick={resetErrorBoundary}>Try Again</button>
+        </div>
+    );
 }
 
 export default ErrorFallback;
